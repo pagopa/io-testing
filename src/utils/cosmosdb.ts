2c59e55398c505f2e8599c7291cee98d3d9738d1
AleDore <alessio85.dore@gmail.com>
2020-08-14 19:02:32 +0200

    fix tls mgmt with local emulator

--- src/utils/cosmosdb.ts
+++ src/utils/cosmosdb.ts
@@ -1,4 +1,5 @@
 import { CosmosClient } from "@azure/cosmos";
+import * as https from "https";
 import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
 
 const cosmosDbUri = getRequiredStringEnv("COSMOSDB_URI");
@@ -7,7 +8,14 @@ const masterKey = getRequiredStringEnv("COSMOSDB_KEY");
 
 const dbName = getRequiredStringEnv("COSMOSDB_NAME");
 
+const tlsRejectUnauthorized = getRequiredStringEnv(
+  "NODE_TLS_REJECT_UNAUTHORIZED"
+);
+
 export const cosmosdbClient = new CosmosClient({
+  agent: new https.Agent({
+    rejectUnauthorized: tlsRejectUnauthorized !== "0"
+  }),
   endpoint: cosmosDbUri as string,
   key: masterKey as string
 });
