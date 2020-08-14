e4879d12da6484c1284e4abb4929bb7b5e56fd6a
AleDore <alessio85.dore@gmail.com>
2020-08-14 15:15:12 +0200

    update over review

--- src/utils/api-props.ts
+++ src/utils/api-props.ts
@@ -2,25 +2,22 @@ import { tryCatch } from "fp-ts/lib/Option";
 import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
 import { NonEmptyString } from "italia-ts-commons/lib/strings";
 
-export const ioFunctionsAppHost: NonEmptyString = tryCatch(() =>
-  getRequiredStringEnv("IO_FUNCTIONS_APP_HOST")
-).getOrElse(process.env.IO_FUNCTIONS_APP_HOST as NonEmptyString);
+export const ioFunctionsAppHost: NonEmptyString = getRequiredStringEnv(
+  "IO_FUNCTIONS_APP_HOST"
+);
 
-export const ioFunctionsAppBasePath: NonEmptyString = tryCatch(() =>
-  getRequiredStringEnv("IO_FUNCTIONS_APP_BASE_PATH")
-).getOrElse(process.env.IO_FUNCTIONS_APP_BASE_PATH as NonEmptyString);
+export const ioFunctionsAppBasePath: NonEmptyString = getRequiredStringEnv(
+  "IO_FUNCTIONS_APP_BASE_PATH"
+);
 
-export const mailHogHost: NonEmptyString = 
-  getRequiredStringEnv("MAILHOG_HOST");
+export const mailHogHost: NonEmptyString = getRequiredStringEnv("MAILHOG_HOST");
 
-export const mailHogSearchApiEndpoint: NonEmptyString = tryCatch(() =>
-  getRequiredStringEnv("MAILHOG_APIV2_SEARCH_ENDPOINT")
-).getOrElse(process.env.MAILHOG_APIV2_SEARCH_ENDPOINT as NonEmptyString);
+export const mailHogSearchApiEndpoint: NonEmptyString = getRequiredStringEnv(
+  "MAILHOG_APIV2_SEARCH_ENDPOINT"
+);
 
-export const mailHogPort: NonEmptyString = tryCatch(() =>
-  getRequiredStringEnv("MAILHOG_PORT")
-).getOrElse(process.env.MAILHOG_PORT as NonEmptyString);
+export const mailHogPort: NonEmptyString = getRequiredStringEnv("MAILHOG_PORT");
 
-export const mailHogApiV1Endpoint: NonEmptyString = tryCatch(() =>
-  getRequiredStringEnv("MAILHOG_APIV1_ENDPOINT")
-).getOrElse(process.env.MAILHOG_APIV1_ENDPOINT as NonEmptyString);
+export const mailHogApiV1Endpoint: NonEmptyString = getRequiredStringEnv(
+  "MAILHOG_APIV1_ENDPOINT"
+);
